03d34d18a4743b3c7854491cbe5ed5737f527647
Vishal kumar <vishalk80269@gmail.com>
2023-12-18 04:25:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Header from "./components/Header";
+import Home from "./Pages/Home";
+import SecureCommunication from "./components/SecureCommunication";
+import IntrusionDetection from "./components/IntrusionDetection";
+import Auth from "./components/Auth";
+import Dashboard from "./components/Dashboard";
+import { Navigate } from "react-router-dom";
 function App() {
+  const [encryptedMessage, setEncryptedMessage] = useState("");
+  const [userRole, setUserRole] = useState("user");
+  const PrivateRoute = ({ children }) => {
+    return userRole === "admin" ? children : <Navigate to="/auth" />;
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header userRole={userRole} setUserRole={setUserRole} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard isAdmin={userRole === "admin"} />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path="/auth"
+            element={
+              <Auth userRole={userRole} onUserRoleChange={setUserRole} />
+            }
+          />
+
+          <Route
+            path="/secureCommunication"
+            element={
+              <SecureCommunication
+                encryptedMessage={encryptedMessage}
+                setEncryptedMessage={setEncryptedMessage}
+              />
+            }
+          />
+          <Route
+            path="/intrusionDetection"
+            element={<IntrusionDetection encryptedMessage={encryptedMessage} />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
